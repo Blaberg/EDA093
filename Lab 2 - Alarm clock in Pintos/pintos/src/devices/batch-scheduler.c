0b64b465e17b67eb810b154ce40db99c05447ec8
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2020-10-13 17:23:54 +0200

    pekare

--- Lab 2 - Alarm clock in Pintos/pintos/src/devices/batch-scheduler.c
+++ Lab 2 - Alarm clock in Pintos/pintos/src/devices/batch-scheduler.c
@@ -29,37 +29,35 @@ int currentDirection = SENDER;
  *	call o
  * */
 typedef struct {
-    int direction;
-    int priority;
+	int direction;
+	int priority;
 } task_t;
 
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
-                    unsigned int num_priority_send, unsigned int num_priority_receive);
+        unsigned int num_priority_send, unsigned int num_priority_receive);
 
 void senderTask(void *);
-
 void receiverTask(void *);
-
 void senderPriorityTask(void *);
-
 void receiverPriorityTask(void *);
 
 
 void oneTask(task_t task);/*Task requires to use the bus and executes methods below*/
-void getSlot(task_t task); /* task tries to use slot on the bus */
-void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
-void leaveSlot(task_t task); /* task release the slot */
+	void getSlot(task_t task); /* task tries to use slot on the bus */
+	void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
+	void leaveSlot(task_t task); /* task release the slot */
 
 
 
 /* initializes semaphores */
-void init_bus(void) {
+void init_bus(void){
 
-    random_init((unsigned int) 123456789);
-    int n, m;
-    for (n = 0; n < 2; n++) {
-        for (m = 0; m < 2; m++) {
+    random_init((unsigned int)123456789);
+    int n,m;
+    for( n = 0; n<2; n++){
+        for( m = 0; m<2; m++){
             cond_init(&waiting[n][m]);
+            &waitingTasks[n][m] = 0;
         }
     }
     space = BUS_CAPACITY;
@@ -80,94 +78,95 @@ void init_bus(void) {
  */
 
 void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
-                    unsigned int num_priority_send, unsigned int num_priority_receive) {
+        unsigned int num_priority_send, unsigned int num_priority_receive)
+{
     unsigned int i;
     /* create sender threads */
-    for (i = 0; i < num_tasks_send; i++)
+    for(i = 0; i < num_tasks_send; i++)
         thread_create("sender_task", 1, senderTask, NULL);
 
     /* create receiver threads */
-    for (i = 0; i < num_task_receive; i++)
+    for(i = 0; i < num_task_receive; i++)
         thread_create("receiver_task", 1, receiverTask, NULL);
 
     /* create high priority sender threads */
-    for (i = 0; i < num_priority_send; i++)
-        thread_create("prio_sender_task", 1, senderPriorityTask, NULL);
+    for(i = 0; i < num_priority_send; i++)
+       thread_create("prio_sender_task", 1, senderPriorityTask, NULL);
 
     /* create high priority receiver threads */
-    for (i = 0; i < num_priority_receive; i++)
-        thread_create("prio_receiver_task", 1, receiverPriorityTask, NULL);
+    for(i = 0; i < num_priority_receive; i++)
+       thread_create("prio_receiver_task", 1, receiverPriorityTask, NULL);
 }
 
 /* Normal task,  sending data to the accelerator */
-void senderTask(void *aux UNUSED) {
-    task_t task = {SENDER, NORMAL};
-    oneTask(task);
+void senderTask(void *aux UNUSED){
+        task_t task = {SENDER, NORMAL};
+        oneTask(task);
 }
 
 /* High priority task, sending data to the accelerator */
-void senderPriorityTask(void *aux UNUSED) {
-    task_t task = {SENDER, HIGH};
-    oneTask(task);
+void senderPriorityTask(void *aux UNUSED){
+        task_t task = {SENDER, HIGH};
+        oneTask(task);
 }
 
 /* Normal task, reading data from the accelerator */
-void receiverTask(void *aux UNUSED) {
-    task_t task = {RECEIVER, NORMAL};
-    oneTask(task);
+void receiverTask(void *aux UNUSED){
+        task_t task = {RECEIVER, NORMAL};
+        oneTask(task);
 }
 
 /* High priority task, reading data from the accelerator */
-void receiverPriorityTask(void *aux UNUSED) {
-    task_t task = {RECEIVER, HIGH};
-    oneTask(task);
+void receiverPriorityTask(void *aux UNUSED){
+        task_t task = {RECEIVER, HIGH};
+        oneTask(task);
 }
 
 /* abstract task execution*/
 void oneTask(task_t task) {
-    getSlot(task);
-    transferData(task);
-    leaveSlot(task);
+  getSlot(task);
+  transferData(task);
+  leaveSlot(task);
 }
 
 
 /* task tries to get slot on the bus subsystem */
-void getSlot(task_t task) {
+void getSlot(task_t task)
+{
     lock_acquire(&mutex);
-    waitingTasks[task.direction][task.priority] += 1;
-    while (space < 1 || task.direction != currentDirection ||
-           (task.priority == NORMAL && (waitingTasks[0][HIGH] || waitingTasks[1][HIGH]))) {
-        cond_wait(&waitingTasks[task.direction][task.priority], &mutex);
+    &waitingTasks[task.direction][task.priority] += 1;
+    while(space < 1 || task.direction !=currentDirection || (task.priority == NORMAL && (waitingTasks[0][HIGH] || waitingTasks[1][HIGH]))) {
+        cond_wait(&waiting[task.direction][task.priority], &mutex);
     }
-    waitingTasks[task.direction][task.priority] -= 1;
+    &waitingTasks[task.direction][task.priority] -= 1;
     space--;
-    currentDirection = task.direction;
+    currentDirection=task.direction;
     lock_release(&mutex);
 
 }
 
 /* task processes data on the bus send/receive */
-void transferData(task_t task) {
+void transferData(task_t task)
+{
     timer_msleep(random_ulong() % 1000);
 }
 
 /* task releases the slot */
-void leaveSlot(task_t task) {
-    lock_acquire(&mutex);
-    space++;
-    if (waitingTasks[currentDirection][HIGH]) {
-        cond_signal(&waiting[currentDirection][HIGH], &mutex);
-    } else if (waitingTasks[1 - currentDirection][HIGH]) {
-        if (space == BUS_CAPACITY) { //Only broadcast if bus is free
-            cond_broadcast(&waiting[1 - currentDirection][HIGH], &mutex);
-        }
-    } else if (waitingTasks[currentDirection][NORMAL]) {
-        cond_signal(&waiting[currentDirection][NORMAL], &mutex);
-    } else if (waitingTasks[1 - currentDirection][NORMAL]) {
-        if (slotsFree == BUS_CAPACITY) { //Only broadcast if bus is free
-            cond_broadcast(&waiting[1 - currentDirection][HIGH], &mutex);
-        }
-    }
-    lock_release(&mutex);
+void leaveSlot(task_t task)
+{
+   lock_acquire(&mutex);
+   space++;
+   if(&waitingTasks[currentDirection][HIGH]){
+       cond_signal(&waiting[currentDirection][HIGH],&mutex);
+   }else if(&waitingTasks[1-currentDirection][HIGH]){
+        cond_signal(&waiting[1-currentDirection][HIGH],&mutex);
+    }else if(&waitingTasks[currentDirection][NORMAL]){
+        cond_signal(&waiting[currentDirection][NORMAL],&mutex);
+    }else if(&waitingTasks[1-currentDirection][NORMAL]){
+        cond_signal(&waiting[1-currentDirection][NORMAL],&mutex);
+    }else{
+    cond_broadcast(&waiting[currentDirection][NORMAL], &mutex);
+}
+lock_release(&mutex);
 
 }
